b76c530b92af928679146a6a742b01a289ea4722
Hao Ly <kevinly0609@gmail.com>
2024-11-04 09:10:47 -0800

    Implemented the code editor

--- Client/components/coding-problem/code-editor/CodeEditor.tsx
+++ Client/components/coding-problem/code-editor/CodeEditor.tsx
@@ -3,15 +3,24 @@ import styles from "./codeEditor.module.css";
 import { CodeEditorProps } from "@/types/interfaces";
 import { Editor } from "@monaco-editor/react";
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ language, initialValue, onChange }): JSX.Element => {
+const CodeEditor: React.FC<CodeEditorProps> = ({
+  language,
+  initialValue,
+  onChange,
+}): JSX.Element => {
   return (
     <div className={styles.code_editor_part}>
       <Editor
-        height="60vh"
-        defaultLanguage={language}
+        height="86vh"
+        language={language}
         defaultValue={initialValue}
         onChange={onChange}
         theme="vs-dark"
+        options={{
+          scrollBeyondLastLine: true,
+          fontSize: 20,
+          fontWeight: "700",
+        }}
       />
     </div>
   );
